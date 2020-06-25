62839f6a21e7481653768f864d71be844d8345ef
sankha <sanjarkalon@gmail.com>
2020-06-26 04:07:00 +0500

    Create

---
+++ src/main/java/uz/sankha/detailsoftheincomingdocument/DetailsOfTheIncomingDocumentApplication.java
@@ -0,0 +1,13 @@
+package uz.sankha.detailsoftheincomingdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailsOfTheIncomingDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetailsOfTheIncomingDocumentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/sankha/detailsoftheincomingdocument/DetailsOfTheIncomingDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sankha.detailsoftheincomingdocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailsOfTheIncomingDocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
